e351627399074876e0e1c804ff0b7d3b935cf5e4
Kevin Tivert <kevintivert@MacBook-Pro.local>
2021-12-15 20:06:34 +0200

    -mFirst table component working

--- client/src/Components/Hero/DashboardHero.tsx
+++ client/src/Components/Hero/DashboardHero.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import CurrencyCard from '../Cards/CurrencyCard';
 import {Icon} from '@iconify/react';
 import Menu from '@iconify/icons-dashicons/menu';
@@ -6,6 +7,7 @@ import InventoryTable from '../Table/InventoryTable';
 import ProductTable from '../Table/ProductTable';
 import BrandTable from '../Table/BrandTable';
 import Table from '../Table/Table';
+
 type dashboardProps={
     EurToUsd: string,
     EurToRwf: string, 
@@ -17,11 +19,60 @@ type dashboardProps={
     onClickProduct: () => void,
     onClickBrand: () => void
 }
+export type brandsApiCallProps ={
+    brand_id: number,
+    brand_name: string,
+    brand_country_of_origin: string,
+    industry: string,
+    date_created: Date,
+}
+export type productsApiCallProps ={
+    product_ref: string,
+    product_name: string,
+    product_brand_id: number,
+    product_category: string,
+    product_price: number
+}
 const DashboardHero = (
     {EurToUsd, EurToRwf, EurToYuan, showInventory, 
         showProduct, showBrand, onClickInventory, 
         onClickProduct, onClickBrand}:dashboardProps) => {
+    const brandTableHeaders:string[] = ["Name", "Industry", "Country Of Origin"];
+    const productTableHeaders: string[] = ["Reference", "Name", "Brand", "Category", "Price"];
+    const [brandData, setBrandData] = useState<brandsApiCallProps[]>();
+    const [productData, setProductData] = useState<productsApiCallProps[]>();
+    useEffect(() => {
+        const axios = require('axios').default;
 
+        const fetchBrandData = async ()=>{ 
+            await axios.get('http://localhost:8080/api/brand')
+                .then((res: any)=>{
+                    //console.log(res);
+                    const data = res.data;
+                    setBrandData(data);
+                    //console.log(brandData);
+                })
+                .catch((err: any) => {
+                    console.error(err);
+                })
+        }
+        fetchBrandData();
+    },[showBrand])
+    useEffect(() => {
+        const axios = require('axios').default;
+        const fetchProductData = async ()=>{ 
+            await axios.get('http://localhost:8080/api/product')
+                .then((res: any)=>{
+                    const data = res.data;
+                    setProductData(data);
+                    //console.log("productData: ",productData);
+                })
+                .catch((err: any) => {
+                    console.error(err);
+                })
+        }
+        fetchProductData();
+    },[showProduct])
     return (
         <>
             <div>
@@ -59,11 +110,12 @@ const DashboardHero = (
                 <section>
                     <div className="tables-view">
                         {showInventory?<InventoryTable/>:null}
-                        {showProduct?<ProductTable/>:null}
-                        {showBrand?<BrandTable/>:null}
-                    </div>
-                    <div>
-                        <Table/>
+                        {/* {showProduct?
+                            <Table colHeaders={productTableHeaders} tableData={productData} showProduct={showProduct}/>
+                        :null} */}
+                        {showBrand?
+                            <Table colHeaders={brandTableHeaders} brandData={brandData} showBrand={showBrand}/>
+                        :null}
                     </div>
                 </section>
             </div>
--- client/src/Components/Table/Table.tsx
+++ client/src/Components/Table/Table.tsx
@@ -1,132 +1,106 @@
-type tabeProps ={
-    colHeaders : JSON,
-    tableData : JSON,
+import { useEffect, useState } from 'react';
+import '../../Styles/Components/Table.scss';
+import {brandsApiCallProps, productsApiCallProps} from '../Hero/DashboardHero';
+
+type tableProps ={
+    colHeaders ?: Array<string>,
+    brandData ?: brandsApiCallProps[],
+    showBrand ?: boolean,
+    showProduct ?: boolean,
+    showInventory ?: boolean
 }
-const Table = () => {
+type brandDataProps ={
+    brand_id: number,
+    brand_name: string,
+    industry: string,
+    brand_country_of_origin: string,
+    product_ref: string,
+    product_name: string,
+    product_brand_id: number,
+    product_category: string,
+    product_price: number
+}
+
+const Table = ({colHeaders, brandData, showBrand, showProduct, showInventory}:tableProps) => {
+    
+    const BrandDataRows = ({brandData}:tableProps)=>{
+        return(
+            <>
+                {brandData && brandData.map((data:brandsApiCallProps)=>(
+                    <tr key={1}>
+                        <td className="table-row-col">
+                            <div className="table-row-col-img-container">
+                                <div className="table-name-container ">
+                                    <div>
+                                        {data.brand_name}
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td className="table-row-col">
+                            <div className="table-row-col-img-container">
+                                <div className="table-name-container ">
+                                    <div>
+                                        {data.industry}
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td className="table-row-col">
+                            <div className="table-row-col-img-container">
+                                <div className="table-name-container ">
+                                    <div>
+                                        {data.brand_country_of_origin}
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td
+                        className="table-delete-container">
+                        <a href="#">
+                            Delete
+                        </a>
+                        </td>
+                    </tr>
+                ))}
+            </>
+        )
+    }
+
+    console.log("Data passed: ",brandData);
     return (
         <>
-            <div className="flex flex-col bg-white my-8 mx-8 text-left">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div
-                        className="
-                        shadow
-                        overflow-hidden
-                        border-b border-gray-200
-                        sm:rounded-lg
-                        "
-                        >
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-50">
-                                        <tr>
-                                        <th scope="col" className="
-                                        px-6
-                                        py-3
-                                        text-left text-xs
-                                        font-medium
-                                        text-gray-500
-                                        uppercase
-                                        tracking-wider
-                                        "
-                                        >
-                                        Name
-                                        </th>
-                                        <th
-                                        scope="col"
-                                        className="
-                                        px-6
-                                        py-3
-                                        text-left text-xs
-                                        font-medium
-                                        text-gray-500
-                                        uppercase
-                                        tracking-wider
-                                        "
-                                        >
+            <div className="table">
+                <div className="table-container">
+                    <div className="table-wrapper">
+                        <div className="table-wrapper-body">
+                            <table className="table-body ">
+                                <thead className="table-header-body">
+                                    <tr>
+                                        {colHeaders&&colHeaders.map((header) =>(
+                                            <th scope="col" className="table-header-name">
+                                                {header}
+                                            </th>
+                                            )
+                                        )}
+                                        {/* <th scope="col" className="table-header-name">
                                         Industry
                                         </th>
-                                        <th
-                                        scope="col"
-                                        className="
-                                        px-6
-                                        py-3
-                                        text-left text-xs
-                                        font-medium
-                                        text-gray-500
-                                        uppercase
-                                        tracking-wider
-                                        "
-                                        >
+                                        <th scope="col" className="table-header-name">
                                         Country of origin
-                                        </th>
-                                        <th scope="col" className="relative px-6 py-3">
-                                        <span className="sr-only">Edit</span>
+                                        </th> */}
+                                        <th scope="col" className="table-header-del-option">
+                                            <span>Delete</span>
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    <tr>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="flex items-center">
-                                                <div className="flex-shrink-0 h-10 w-10">
-                                                    <img
-                                                    className="h-10 w-10 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1619914775389-748e5e136c26?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIwMTk4MjAw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100"
-                                                    alt=""
-                                                    />
-                                                </div>
-                                                <div className="ml-4">
-                                                    <div className="text-sm font-medium text-gray-900">
-                                                        Flora Wu
-                                                    </div>
-                                                    <div className="text-sm text-gray-500">
-                                                        flora.wu@example.com
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                        <div className="text-sm text-gray-900">Software engineer</div>
-                                        <div className="text-sm text-gray-500">IT</div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                        <span
-                                        className="
-                                        px-2
-                                        inline-flex
-                                        text-xs
-                                        leading-5
-                                        font-semibold
-                                        rounded-full
-                                        bg-green-100
-                                        text-green-800
-                                        "
-                                        >
-                                        Active
-                                        </span>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        Admin
-                                        </td>
-                                        <td
-                                        className="
-                                        px-6
-                                        py-4
-                                        whitespace-nowrap
-                                        text-right text-sm
-                                        font-medium
-                                        "
-                                        >
-                                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                            Edit
-                                        </a
-                                        >
-                                        </td>
-                                    </tr>
+                                <tbody className="table-body">
+                                    {showBrand? <BrandDataRows brandData={brandData}/>:null}
+                                    
                                 </tbody>
                             </table>
                         </div>
-                        </div>
+                    </div>
                 </div>
             </div>
         </>
